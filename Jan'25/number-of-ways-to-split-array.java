084d05e8a2e5ab52fb115b22d87c8b76f614c99c
Karthick <karthickkumarmoorthys@gmail.com>
2025-01-03 08:10:02 +0530

    3/4/12

---
+++ Jan'25/number-of-ways-to-split-array.java
@@ -0,0 +1,20 @@
+// https://leetcode.com/problems/number-of-ways-to-split-array/description/
+
+class Solution {
+    public int waysToSplitArray(int[] nums) {
+        long p=0,s=0;
+        int ans=0;
+        for(int x:nums){
+            s+=x;
+        }
+        for(int i=0;i<nums.length-1;i++){
+            int x=nums[i];
+            p+=x;
+            s-=x;
+            if(p>=s){
+                ans+=1;
+            }
+        }
+        return ans;
+    }
+}
