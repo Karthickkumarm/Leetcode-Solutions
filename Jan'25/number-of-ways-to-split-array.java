af5db451c8744c6085974554eb9e5fc39fcb77fe
Karthick <karthickkumarmoorthys@gmail.com>
2025-01-03 08:10:52 +0530

    3/1/25

--- Jan'25/number-of-ways-to-split-array.java
+++ Jan'25/number-of-ways-to-split-array.java
@@ -2,17 +2,17 @@
 
 class Solution {
     public int waysToSplitArray(int[] nums) {
-        long p=0,s=0;
-        int ans=0;
-        for(int x:nums){
-            s+=x;
+        long p = 0, s = 0;
+        int ans = 0;
+        for (int x : nums) {
+            s += x;
         }
-        for(int i=0;i<nums.length-1;i++){
-            int x=nums[i];
-            p+=x;
-            s-=x;
-            if(p>=s){
-                ans+=1;
+        for (int i = 0; i < nums.length - 1; i++) {
+            int x = nums[i];
+            p += x;
+            s -= x;
+            if (p >= s) {
+                ans += 1;
             }
         }
         return ans;
